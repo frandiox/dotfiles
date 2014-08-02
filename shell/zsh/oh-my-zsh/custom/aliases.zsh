0f601503f9c67a795530292345076dd016b159b7
Fran Dios <frankdiox@gmail.com>
2014-08-02 15:48:27 +0200

    Added oh-my-zsh to dotfiles

---
+++ shell/zsh/oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,21 @@
+
+#vim
+alias v='vim'
+alias vza='vim $ZSH_CUSTOM/aliases.zsh'
+
+# Ranger alias
+alias r='ranger'
+
+#awk - grep
+alias ack='$HOME/bin/ack'
+
+# ToDo
+alias todo='$EDITOR $HOME/.todo'
+
+# rm alias
+alias rm='rm'
+
+# Connection speed test
+alias speedtest='curl http://cachefly.cachefly.net/100mb.test > /dev/null'
+
+
